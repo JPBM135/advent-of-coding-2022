e36662ca4ed6072048c836ff6c52885e297a331c
João Pedro Borges Martins <jpedrobm0@gmail.com>
2022-12-03 17:37:19 -0300

    fix(lint): linting

--- Day 02/index.js
+++ Day 02/index.js
@@ -17,13 +17,13 @@ const antTable = {
 const scoreArray = ['Loss', 'Draw', 'Win'];
 
 const scores = {
+	enemy: 0,
 	player: 0,
-	self: 0,
 };
 
 function moveToType(move) {
 	const entries = Object.entries(table);
-	return entries.find((val) => val[1].indexOf(move) >= 0)[0];
+	return entries.find((val) => val[1].includes(move))[0];
 }
 
 function calcScore(move, result) {
@@ -37,48 +37,48 @@ function calcScore(move, result) {
 
 // Part 1
 
-for (const [player, self] of rounds) {
-	const moveArray = [moveToType(player), moveToType(self)];
+for (const [enemy, player] of rounds) {
+	const moveArray = [moveToType(enemy), moveToType(player)];
 	const resultArray = [];
 
-	// console.log(playerMove, selfMove);
+	// console.log(enemyMove, playerMove);
 
 	if (moveArray[0] === moveArray[1]) {
 		resultArray.push('Draw', 'Draw');
-	} else if (Object.entries(antTable).find(([p, s]) => p === moveArray[0] && s === moveArray[1])) {
+	} else if (Object.entries(antTable).some(([enm, ply]) => enm === moveArray[0] && ply === moveArray[1])) {
 		resultArray.push('Loss', 'Win');
 	} else {
 		resultArray.push('Win', 'Loss');
 	}
 
-	Object.keys(scores).forEach((val, idx) => {
+	for (const [idx, val] of Object.keys(scores).entries()) {
 		scores[val] += calcScore(moveArray.at(idx), resultArray.at(idx));
-	});
-}
-
-console.log(['Result of the game', `You: ${scores.self}`, `Player: ${scores.player}`].join('\n'));
-
-// Part 2
-
-scores.player = 0;
-scores.self = 0;
-
-function findTheRightMove(player, self) {
-	switch (self) {
-		case 'X':
-			return Object.entries(antTable).filter(([_, move]) => move === player)[0][0];
-		case 'Y':
-			return player;
-		case 'Z':
-			return Object.entries(antTable).filter(([move]) => move === player)[0][1];
 	}
 }
 
-for (const [player, self] of rounds) {
-	const moveArray = [moveToType(player), findTheRightMove(moveToType(player), self)];
+console.log(['Result of the game', `You: ${scores.player}`, `enemy: ${scores.enemy}`].join('\n'));
+
+// Part 2
+
+scores.enemy = 0;
+scores.player = 0;
+
+function findTheRightMove(enemy, player) {
+	switch (player) {
+		case 'X':
+			return Object.entries(antTable).find(([_, move]) => move === enemy)[0];
+		case 'Y':
+			return enemy;
+		case 'Z':
+			return Object.entries(antTable).find(([move]) => move === enemy)[1];
+	}
+}
+
+for (const [enemy, player] of rounds) {
+	const moveArray = [moveToType(enemy), findTheRightMove(moveToType(enemy), player)];
 	const resultArray = [];
 
-	switch (self) {
+	switch (player) {
 		case 'X':
 			resultArray.push('Win', 'Loss');
 			break;
@@ -88,12 +88,14 @@ for (const [player, self] of rounds) {
 		case 'Z':
 			resultArray.push('Loss', 'Win');
 			break;
+		default:
+			throw new Error(`Unknown type ${player}`);
 	}
 
-	Object.keys(scores).forEach((val, idx) => {
+	for (const [idx, val] of Object.keys(scores).entries()) {
 		// console.log(moveArray.at(idx), resultArray.at(idx), calcScore(moveArray.at(idx), resultArray.at(idx)));
 		scores[val] += calcScore(moveArray.at(idx), resultArray.at(idx));
-	});
+	}
 }
 
-console.log(['Result of Part 2', `You: ${scores.self}`, `Player: ${scores.player}`].join('\n'));
+console.log(['Result of Part 2', `You: ${scores.player}`, `enemy: ${scores.enemy}`].join('\n'));
