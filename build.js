42f2a60369adbc74c3b45578ca8016937d995a3e
João Pedro Borges Martins <jpedrobm0@gmail.com>
2022-12-04 20:25:28 -0300

    fix(build): ENOENT error

--- build.js
+++ build.js
@@ -1,6 +1,6 @@
 import { spawnSync } from 'node:child_process';
+import path from 'node:path';
 import process from 'node:process';
-
 // Util script to build the project of the specified day
 
 const args = process.argv.slice(2)[0];
@@ -10,8 +10,23 @@ if (!args?.length) {
 	process.exit(1);
 }
 
+const shell =
+	process.platform === 'win32' ? 'C:\\Windows\\SysWOW64\\WindowsPowerShell\\v1.0\\powershell.exe' : undefined;
+
 console.log(`Building Day ${args}...`);
-spawnSync('swc', [`"./Day ${args}"`, '--out-dir', './dist'], { stdio: 'inherit' });
+
+const output = spawnSync('swc.cmd', [`"./Day ${args}"`, '--out-dir', './dist'], {
+	shell,
+	stdio: 'inherit',
+});
+
+if (output.error) {
+	console.error(output.error);
+	process.exit(1);
+}
+
+// console.log(output.output.toString());
+
 console.log(`Done building Day ${args}!`);
 
 process.exit(0);
