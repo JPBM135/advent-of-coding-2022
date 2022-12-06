6b34538e3201a82cfe74973b7e8a36d2819ca993
João Pedro Borges Martins <jpedrobm0@gmail.com>
2022-12-06 14:38:23 -0300

    feat: Day 06

---
+++ Day 06/index.ts
@@ -0,0 +1,20 @@
+import { input } from './input.js';
+
+const chunks = input.split('');
+
+function findMarker(input: string[], markerSize: number) {
+	let charSize = 0;
+	const circular: string[] = [];
+
+	for (const char of input) {
+		if (circular.length >= markerSize) circular.shift();
+		circular.push(char);
+		charSize++;
+		if (circular.length === markerSize && circular.length === new Set(circular).size) break;
+	}
+
+	return charSize;
+}
+
+console.log(['Part 01:', 'Found packet marker at:', findMarker(chunks, 4)].join(' '));
+console.log(['Part 01:', 'Found message marker at:', findMarker(chunks, 14)].join(' '));
