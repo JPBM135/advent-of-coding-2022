06c466d61fe95786f4905fa051c08c050067792f
João Pedro Borges Martins <jpedrobm0@gmail.com>
2022-12-07 20:51:15 -0300

    chore: inspect default options

--- logger/logger.ts
+++ logger/logger.ts
@@ -1,6 +1,10 @@
+import { inspect } from 'node:util';
 import kleur from 'kleur';
 
 kleur.enabled = true;
+
+inspect.defaultOptions.depth = 10;
+inspect.defaultOptions.maxArrayLength = Number.POSITIVE_INFINITY;
 
 export function success(day: number, part: number, response: string) {
 	console.log(`${kleur.green(`[Day ${day}] Part ${part}:`)} ${response}`);
