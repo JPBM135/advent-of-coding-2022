ee6f19a6b30f8ef73d09d1fea009dc0ae7b401da
João Pedro Borges Martins <jpedrobm0@gmail.com>
2022-12-08 13:00:26 -0300

    feat: log file

--- util/logger.ts
+++ util/logger.ts
@@ -1,7 +1,10 @@
+import { createWriteStream } from 'node:fs';
 import { inspect } from 'node:util';
 import kleur from 'kleur';
 
 kleur.enabled = true;
+
+const logStream = createWriteStream('log.log', { flags: 'a' });
 
 inspect.defaultOptions.depth = 10;
 inspect.defaultOptions.maxArrayLength = Number.POSITIVE_INFINITY;
@@ -18,13 +21,18 @@ const Logger: ILogger = {
 	debugEnabled: true,
 	infoEnabled: true,
 	success(day: number, part: number, response: string) {
+		logStream.write(`\n${kleur.green(`[Day ${day}] Part ${part}:`)} ${response}`);
 		console.log(`${kleur.green(`[Day ${day}] Part ${part}:`)} ${response}`);
 	},
 	info(message: string, ...args: any[]) {
+		logStream.write(
+			`\n${this.infoEnabled ? '' : '[DISABLED]'}${kleur.blue('[Info]:')} ${message} ${args.map((val) => inspect(val))}`,
+		);
 		if (!this.infoEnabled) return;
 		console.log(`${kleur.blue('[Info]:')} ${message}`, ...args);
 	},
 	debug(message: string, ...args: any[]) {
+		logStream.write(`\n${this.debugEnabled ? '' : '[DISABLED]'}${kleur.gray('[Debug]:')} ${message}`);
 		if (!this.debugEnabled) return;
 		console.log(`${kleur.gray('[Debug]:')} ${message}`, ...args);
 	},
